517eda5dcec55f7fc4bfe8a622c545af34231547
Ranjan <skillsapphire@gmail.com>
2022-03-27 23:11:11 +0200

    code

--- src/components/profile/UpdatePassword.js
+++ src/components/profile/UpdatePassword.js
@@ -9,18 +9,18 @@ import './UpdatePassword.css'
 function UpdatePassword() {
 
     const { state, dispatch } = useContext(UserContext);
-    const [ loading, setLoading ] = useState(false);
-    const [ password, setPassword ] = useState();
-    const [ confirmPassword, setConfirmPassword ] = useState();
+    const [loading, setLoading] = useState(false);
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
     const navigate = useNavigate();
-    
+
 
     const onSubmit = (event) => {
 
         event.preventDefault();
         setLoading(true);
 
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             alert("Password dont match");
             return;
         }
@@ -37,63 +37,63 @@ function UpdatePassword() {
                 "password": password,
             };
             axios.patch(url, requestData, config)
-            .then((response) => {
-                setPassword("");
-                setConfirmPassword("");
-                alert("Password updated successfully!")
-                setLoading(false);
-                navigate('/');
-                
-            })
-            .catch((error) => {
-                setLoading(false);
-                console.log(error);
-            });
+                .then((response) => {
+                    setPassword("");
+                    setConfirmPassword("");
+                    alert("Password updated successfully!")
+                    setLoading(false);
+                    navigate('/');
+
+                })
+                .catch((error) => {
+                    setLoading(false);
+                    console.log(error);
+                });
 
         }
     }
 
-  return (
-    <div className='update-password-container mx-auto  mt-2'>
-        {state == null ? <div>Please login</div> :
-        <div className='p-3'>
-            <div className="row mb-2">
-                <div className="col-md-12 col-sm-12">
-                    <h5>Update Password</h5>
+    return (
+        <div className='update-password-container mx-auto  mt-2'>
+            {state == null ? <div>Please login</div> :
+                <div className='p-3'>
+                    <div className="row mb-2">
+                        <div className="col-md-12 col-sm-12">
+                            <h5>Change Password</h5>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 col-sm-12">
+                            <form onSubmit={(event) => { onSubmit(event) }}>
+                                <div className='row'>
+                                    <div className='col-sm-12 mb-3'>
+                                        <input value={password} onChange={(ev) => { setPassword(ev.target.value) }}
+                                            placeholder='Enter New Password'
+                                            type="password"
+                                            className="form-control"
+                                            required />
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className='col-sm-12 mb-3'>
+                                        <input value={confirmPassword} onChange={(ev) => { setConfirmPassword(ev.target.value) }}
+                                            placeholder='Confirm New Password'
+                                            type="password"
+                                            className="form-control" required />
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className='col-sm-12 mb-3'>
+                                        <input type="submit" className='btn btn-primary' value="Submit" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-md-12 col-sm-12">
-                    <form onSubmit={(event) => { onSubmit(event) }}>
-                        <div className='row'>
-                            <div className='col-sm-12 mb-3'>
-                                <input value={password} onChange={(ev) => { setPassword(ev.target.value) }} 
-                                    placeholder='Password' 
-                                    type="password" 
-                                    className="form-control"
-                                    required />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className='col-sm-12 mb-3'>
-                                <input value={confirmPassword} onChange={(ev) => { setConfirmPassword(ev.target.value) }} 
-                                    placeholder='Confirm Password' 
-                                    type="password" 
-                                    className="form-control" required />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className='col-sm-12 mb-3'>
-                                <input type="submit" className='btn btn-primary' value="Submit" />
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            }
         </div>
-        }
-    </div>
-  )
+    )
 }
 
 export default UpdatePassword
